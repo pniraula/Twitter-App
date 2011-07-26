2a875a0e081c6f9aa80e0547b8f52f4dc04a98e8
prabesh <prabesh@prabesh.(none)>
2011-07-26 05:07:54 -0500

    made title somewhat dynamic by editing pages_controller.rb and application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+ #makes title somewhat dynamic/ reduces repetations 
+ def title
+   base_title = "Twitter-App"
+   if @title.nil?
+	base_title
+   else
+	"#{base_title} | #{@title}"
+   end
+ end
 end
